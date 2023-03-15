e938166953508becbdaf735c14e6c5276e9d08f7
TronkyBass <121621363+TronkyBass@users.noreply.github.com>
2023-03-15 12:38:43 -0300

    }

--- Python ja/Bucles/Literar_listas.py
+++ Python ja/Bucles/Literar_listas.py
@@ -1,3 +1,3 @@
-Animales= ["perro", "gato", "loro", "cocodrilo"]
+Animales = ["perro", "gato", "loro", "cocodrilo"]
 for animal in animales:
     print(f"ahora la variable es igual a:{animalel}")
---
+++ Python ja/pandas/pandas.py
@@ -0,0 +1,2 @@
+import pandas as pd
+archivo = pd-read_csv("")
